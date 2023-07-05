dfadb18206521becca8d32d66c1038e46056c93f
Aditya <adityanandy2802@gmail.com>
2023-07-06 02:54:37 +0530

    Done

--- MCMC.py
+++ MCMC.py
@@ -9,6 +9,9 @@ import math
 from tqdm import trange
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.stats import multivariate_normal, norm
+import hamiltorch
+import torch.distributions as dist
 import streamlit as st
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
@@ -102,9 +105,38 @@ def bivariate_normal_pdf(x, y, mean, covariance):
 
     return pdf
 
+def log_prob(theta):
+  for i in range(theta.squeeze().shape[0]):
+    if i == 0:
+      a = theta.squeeze()[i] * pow(x, theta.squeeze().shape[0] - 1 - i)
+    else:
+      a += theta.squeeze()[i] * pow(x, theta.squeeze().shape[0] - 1 - i)
+  return dist.Normal(a, 1).log_prob(y).sum()
+
+def log_prior(theta):
+  return dist.Normal(0, 1).log_prob(theta).sum()
+
+def log_posterior(theta):
+  return log_prob(theta) + log_prior(theta)
+
+def polynomial_samples_list(x, y, min, max, step):
+  init = (torch.rand(degree + 1,) * (max - min) + min).to(device)
+  list_ = init.unsqueeze(0)
+  init0 = init
+  mybar = st.progress(0, text = "")
+  for i in trange(10000):
+    init = dist.Normal(init0, 1).sample((1,))
+    alpha = float((log_posterior(init) - log_posterior(init0)).exp())
+    u = random.uniform(0,1)
+    if (alpha > u):
+      list_ = torch.cat((list_, init.squeeze().unsqueeze(0)), axis = 0)
+      init0 = init
+    mybar.progress(float(i / 10000))
+  return list_
+
 st.header("MCMC")
 
-options = ["Linear Regression", "Uni-Variate Rejection Sampling", "Bi-Variate Rejection Sampling"]
+options = ["Linear Regression", "Uni-Variate Rejection Sampling", "Bi-Variate Rejection Sampling", "Polynomial Regression"]
 task = st.multiselect("Select Task", options, max_selections = 1)
  
 if "Linear Regression" in task:
@@ -210,3 +242,99 @@ if "Bi-Variate Rejection Sampling" in task:
         plt.legend(loc = "best")
         st.pyplot()
 
+if "Polynomial Regression" in task:
+  degree = st.slider("Degree: ", 1, 10, step = 1)
+  play = st.button("Play", type = "primary", use_container_width = True)
+  if play:
+    x = torch.linspace(-3, 3, 100).to(device)
+    ground_truth_params = torch.randint(-10, 10, (degree + 1, ))
+    ground_truth = torch.tensor([])
+    for i in range(ground_truth_params.shape[0]):
+      if i == 0:
+        ground_truth = ground_truth_params[i] * torch.pow(x, int(ground_truth_params.shape[0]) - 1 - i)
+      else:
+        ground_truth += ground_truth_params[i] * torch.pow(x, int(ground_truth_params.shape[0]) - 1 - i)
+    eps = torch.randn_like(x) * 1.0
+    y = ground_truth + eps
+    plt.plot(x.cpu(), ground_truth.cpu(), label = "Ground Truth")
+    plt.scatter(x.cpu(), y.cpu(), label = "Data")
+    plt.legend(loc = "best")
+    plt.show()
+    st.pyplot()
+
+    st.header("Results using Hamiltorch")
+    x0 = torch.randint(-20, 20, (degree + 1, )).type(torch.float)
+    num_samples = 1000
+    params_hmc = hamiltorch.sample(log_prob_func = log_posterior, params_init = x0, num_samples = num_samples, num_steps_per_sample= 5, step_size = 0.01)
+    params_hmc = torch.stack(params_hmc)
+    # Plot the traces corresponding to the two parameters
+    fig, axes = plt.subplots(x0.shape[0], 1, sharex=True)
+
+    for i, param_vals in enumerate(params_hmc.T):
+        axes[i].plot(param_vals, label='Trace')
+        axes[i].set_xlabel('Iteration')
+        axes[i].set_ylabel(fr'$\theta_{i}$')
+
+    # Plot the true values as well
+    for i, param_vals in enumerate(ground_truth_params):
+        axes[i].axhline(param_vals, color='C1', label='Ground truth')
+        axes[i].legend()
+    st.pyplot()
+
+    # Plot the posterior predictive distribution
+    plt.figure()
+    plt.scatter(x.cpu(), y.cpu(), label='Data', color='C0')
+    plt.plot(x.cpu(), ground_truth.cpu(), label='Ground truth', color='C1', linestyle='--')
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+    # Get posterior samples. Thin first 100 samples to remove burn-in
+    posterior_samples = params_hmc[30:]
+    for i in range(posterior_samples.shape[1]):
+      if i == 0:
+        y_hat = posterior_samples[:, i].unsqueeze(1) * torch.pow(x.cpu(), posterior_samples.shape[1] - 1 - i)
+      else:
+        y_hat += posterior_samples[:, i].unsqueeze(1) * torch.pow(x.cpu(), posterior_samples.shape[1] - 1 - i)
+    # Plot mean and 95% confidence interval
+
+    plt.plot(x.cpu(), y_hat.mean(axis=0), label='Mean', color='C2')
+    plt.fill_between(x.cpu(), y_hat.mean(axis=0) - 2 * y_hat.std(axis=0), y_hat.mean(axis=0) + 2 * y_hat.std(axis=0), alpha=0.5, label='95% CI', color='C2')
+    plt.legend()
+    st.pyplot()
+
+    st.header("Results from Scratch")
+    list_ = polynomial_samples_list(x, y, -10, 10, 2)
+
+    fig, axes = plt.subplots(degree + 1, 1, sharex=True)
+
+    for i, param_vals in enumerate(list_.cpu().T):
+        axes[i].plot(param_vals, label='Trace')
+        axes[i].set_xlabel('Iteration')
+        axes[i].set_ylabel(fr'$\theta_{i}$')
+
+    # Plot the true values as well
+    for i, param_vals in enumerate(ground_truth_params):
+        axes[i].axhline(param_vals, color='C1', label='Ground truth')
+        axes[i].legend()
+    st.pyplot()
+
+    # Plot the posterior predictive distribution
+    plt.figure()
+    plt.scatter(x.cpu(), y.cpu(), label='Data', color='C0')
+    plt.plot(x.cpu(), ground_truth.cpu(), label='Ground truth', color='C1', linestyle='--')
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+    # Get posterior samples. Thin first 100 samples to remove burn-in
+    posterior_samples = list_.cpu()[30:]
+    for i in range(posterior_samples.shape[1]):
+      if i == 0:
+        y_hat = posterior_samples[:, i].unsqueeze(1) * torch.pow(x.cpu(), posterior_samples.shape[1] - 1 - i)
+      else:
+        y_hat += posterior_samples[:, i].unsqueeze(1) * torch.pow(x.cpu(), posterior_samples.shape[1] - 1 - i)
+    # Plot mean and 95% confidence interval
+
+    plt.plot(x.cpu(), y_hat.mean(axis=0), label='Mean', color='C2')
+    plt.fill_between(x.cpu(), y_hat.mean(axis=0) - 2 * y_hat.std(axis=0), y_hat.mean(axis=0) + 2 * y_hat.std(axis=0), alpha=0.5, label='95% CI', color='C2')
+    plt.legend()
+    st.pyplot()
